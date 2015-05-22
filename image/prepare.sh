f9ab8e913540ca1d02e6ecb8e0ba4d4e8a147bf1
Emanuele Peruffo <info@emanueleperuffo.com>
2015-05-22 16:22:48 +0200

    first commit. need tls

---
+++ image/prepare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+source /build/buildconfig
+set -x
+
+## Temporarily disable dpkg fsync to make building faster.
+if [[ ! -e /etc/dpkg/dpkg.cfg.d/docker-apt-speedup ]]; then
+	echo force-unsafe-io > /etc/dpkg/dpkg.cfg.d/docker-apt-speedup
+fi
+
+export INITRD=no
+
+# Update repos
+apt-get update
+
+## Upgrade all packages.
+apt-get dist-upgrade -y --no-install-recommends
+
+chmod +x /build/runit/*
+chmod +x /build/*.sh
